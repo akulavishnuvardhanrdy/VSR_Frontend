b5a7ada354dfd329cf42c53b244d3e66a84dff2e
Vishnu <akulavishnuvardhanrdy@gmail.com>
2024-12-28 17:54:51 +0530

    Environment Variables Taking Success and the Connection to DB Success

--- src/Backend/BackendServer.js
+++ src/Backend/BackendServer.js
@@ -3,23 +3,16 @@ import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import process from 'process'; 
-import BookingDetailsRoutes from '../API_Routes/BookingDetailsRoutes'
-import LoginDetailsRoutes from '../API_Routes/LoginDetailsRoutes'
 
-dotenv.config();
+dotenv.config(); 
 const app = express();
-const port = process.env.PORT || 5000;
-
+const mongoURI = process.env.MONGO_URI;
+const port = process.env.PORT || 4000; 
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(mongoURI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
-
-
-app.use('/booking', BookingDetailsRoutes);
-app.use('/addUsers', LoginDetailsRoutes);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
